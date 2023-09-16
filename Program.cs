a6e4a58f91e05ca5bae027047a4c4e7fdd00a26a
azharazharuddin22 <azharazharuddin22@gmail.com>
2023-09-16 21:29:15 +0530

    this is leap year code

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace leapyear
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a number:");
+            int leapyear=int.Parse(Console.ReadLine());
+            if ((leapyear % 4 == 0 && leapyear % 100 != 0) || (leapyear % 400 == 0))
+                Console.WriteLine($"{leapyear}is a leap year");
+            else
+                Console.WriteLine($"{leapyear} is a not leap year");
+            Console.ReadLine();
+
+
+            
+        }
+    }
+}
